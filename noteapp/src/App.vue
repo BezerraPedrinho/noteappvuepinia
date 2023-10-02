b54399ce9a8137f97cd2aae1a6b8282dde4ee4bd
BezerraPedrinho <phdesign9@gmail.com>
2023-10-02 10:02:21 -0300

    add pinia

---
+++ noteapp/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Toolbar from "./components/commom/ToolBar.vue";
+</script>
+
+<template>
+  <div class="rs__notes-wrapper"></div>
+  <Toolbar />
+</template>
---
+++ noteapp/src/components/commom/ToolBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="rs__top-toolbar">
+    <div class="rs__tollbar-title">RS Notes</div>
+  </div>
+</template>
+<script setup></script>
